46ccd995fe0c3be08b4382e8a492f2155aa5627c
yatendra <yatendra@singsys.com>
2022-02-02 18:55:54 +0530

    fixed  eslint & prettier config

--- admin/registerSW.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/vite-vuetify-tailwind-3/admin/sw.js', {
-      scope: '/vite-vuetify-tailwind-3/admin/'
-    })
-  })
-}
