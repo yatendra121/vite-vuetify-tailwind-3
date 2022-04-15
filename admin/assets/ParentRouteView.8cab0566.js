b51966d05fa040cc9096887c04db0dae0c4993a8
yatendra <yatendra@singsys.com>
2022-04-15 15:30:21 +0530

    fixed

---
+++ admin/assets/ParentRouteView.8cab0566.js
@@ -0,0 +1,1 @@
+import{_ as r,i as t,o as n,a}from"./index.e0d2f4fa.js";const c={};function s(e,_){const o=t("router-view");return n(),a(o,{key:e.$route.fullPath})}var f=r(c,[["render",s]]);export{f as default};
---
+++ admin/assets/index.b7d921b2.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.e0d2f4fa.js";const e={};function n(_,c){return null}var a=r(e,[["render",n]]);export{a as default};
