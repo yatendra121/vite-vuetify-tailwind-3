ac2ddd75eba3a59ecb3b754ea9723090c6ce1be6
yatendra121 <yatendra121kushwaha@gmail.com>
2024-12-06 14:57:46 +0530

    fix: test config file on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -104,7 +104,7 @@ export default defineConfig({
         minify: 'terser',
         terserOptions: {
             compress: {
-                drop_console: true,
+                drop_console: false,
                 drop_debugger: false
             }
         }
